2f364d79d5ebb4cac839fcb11ef9f5d8bb72f101
tsarpaul <21146452+tsarpaul@users.noreply.github.com>
2020-09-17 13:42:17 +0000

    Small adjustments

---
+++ src/prod/ktl/setup.sh
@@ -0,0 +1,3 @@
+sudo apt-get install liblttng-ust-dev 
+sudo apt-get install uuid-dev 
+sudo apt-get install libc++1 multiarch-support libc6 libc++-dev libc++-helpers libc++-test libc++abi-dev libc++abi-test libc++abi1
